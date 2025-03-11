ff8f0d871199368f5b5c2e7f556d0d5fd30cf21a
Christelle Revol <christellerevol.works@gmail.com>
2025-03-11 15:25:42 +0100

    creation bookings index sans booking dans db

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,10 @@
 class BookingsController < ApplicationController
   def index
     @bookings = Booking.all
+
+    # séparer bookings en 2 arrays
+    # un où characters.user == current_user
+    # un avec le reste
+    # puis 2 each dans view pour séparer rentals et demandes d'autres users
   end
 end
