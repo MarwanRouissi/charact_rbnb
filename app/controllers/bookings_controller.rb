058061cbeeda3cb6ed75a68d8bd611bd50294167
Christelle Revol <christellerevol.works@gmail.com>
2025-03-12 12:48:25 +0100

    ajout filtrage booking et début update status

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,6 +2,8 @@ class BookingsController < ApplicationController
   def index
     @bookings = Booking.all
 
+    @user_bookings = @bookings.select { |booking| booking.user == current_user }
+    @bookings_to_review = @bookings.select { |booking| booking.character.user == current_user }
     # séparer bookings en 2 arrays
     # un où characters.user == current_user
     # un avec le reste
@@ -13,6 +15,30 @@ class BookingsController < ApplicationController
     @booking = Booking.new(params_booking)
     @booking.user = current_user
     @booking.character = @character
+    saving_booking
+  end
+
+  def update
+    @booking = Booking.find(params[:id])
+
+    if @booking.update(book_status: book_status_value)
+      redirect_to bookings_path
+    else
+      redirect_to bookings_path, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def book_status_value
+    case params[:book_status]
+    when "accepted" then true
+    when "declined" then false
+    when "pending" then nil
+    end
+  end
+
+  def saving_booking
     if @character.user == current_user
       flash[:alert] = "sorry but you cannot rent your own characters"
       render 'characters/show', status: :unprocessable_entity
@@ -23,9 +49,11 @@ class BookingsController < ApplicationController
     end
   end
 
-  private
-
   def params_booking
     params.require(:booking).permit(:start_date, :end_date)
+  end
+
+  def params_booking_update
+    params.require(:booking).permit(:book_status)
   end
 end
