60ff209aeebfd8779d4dabc511a6342629b0cace
Quentin Aubert <quentinaubert@bluewin.ch>
2025-03-12 12:26:31 +0100

    added form to create new character with validations and images using cloudinary

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,4 +1,8 @@
 class Character < ApplicationRecord
   belongs_to :user
   has_many :users, through: :bookings
+  validates :name, presence: true, uniqueness: true
+  validates :description, presence: true
+  validates :category, presence: true
+  has_one_attached :photo
 end
